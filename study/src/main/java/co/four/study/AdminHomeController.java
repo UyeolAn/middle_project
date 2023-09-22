100112d52b437350f6d00559222d3e137c6005d5
Song Min Ju <97097194+minju-song@users.noreply.github.com>
2023-09-22 16:23:32 +0900

    홈 조금 수정

--- study/src/main/java/co/four/study/AdminHomeController.java
+++ study/src/main/java/co/four/study/AdminHomeController.java
@@ -10,6 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import co.four.study.common.ViewResolve;
+import co.four.study.course.service.CourseService;
+import co.four.study.course.service.CourseVO;
+import co.four.study.course.serviceImpl.CourseServiceImpl;
 import co.four.study.member.service.MemberService;
 import co.four.study.member.service.MemberVO;
 import co.four.study.member.serviceImpl.MemberServiceImpl;
@@ -35,6 +38,7 @@ public class AdminHomeController extends HttpServlet {
 		QuestionService qdao = new QuestionServiceImpl();
 		MemberService mdao = new MemberServiceImpl();
 		MemberCourseService mcdao = new MemberCourseServiceImpl();
+		CourseService cdao = new CourseServiceImpl();
 		
 		int unanswerQ = qdao.unanswerQuestion();
 		
@@ -43,9 +47,13 @@ public class AdminHomeController extends HttpServlet {
 		
 		int totalP = mcdao.totalPrice();
 		
+		List<CourseVO> clist = cdao.courseSelectList(null);
+		int courseCount = clist.size();
+		
 		request.setAttribute("newQ", unanswerQ);
 		request.setAttribute("members", memberCount);
 		request.setAttribute("totalP", totalP);
+		request.setAttribute("courses", courseCount);
 		
 		String page = "admin/home/home";
 		ViewResolve.foward(request, response, page);
