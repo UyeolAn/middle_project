d86c5ab300bd677946d921c2ec4d3c339a02e1ca
CHUNcarry <chuncarry00@gmail.com>
2023-09-22 16:25:41 +0900

    비밀번호 수정중

--- study/src/main/java/co/four/study/member/web/AjaxPasswordResetTarget.java
+++ study/src/main/java/co/four/study/member/web/AjaxPasswordResetTarget.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -22,38 +23,40 @@ import co.four.study.member.serviceImpl.MemberServiceImpl;
 @WebServlet("/ajaxpasswordresettarget.do")
 public class AjaxPasswordResetTarget extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-  //비밀번호 재설정 페이지 이동
-    public AjaxPasswordResetTarget() {
-        super();
-       
-    }
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		MemberService dao = new MemberServiceImpl();
-		MemberVO vo = new MemberVO();
-		ObjectMapper objectMapper = new ObjectMapper();
-		
-		vo.setMemberId(request.getParameter("memberId"));
-		vo.setMemberEmail(request.getParameter("memberEmail"));
-		dao.memberSearchPass(vo);
+	// 비밀번호 재설정 대상 탐색
+	public AjaxPasswordResetTarget() {
+		super();
 
-		Map<String, String> result = new HashMap<>();
-
-		result.put("memberId",vo.getMemberId());
-		result.put("memberEmail",vo.getMemberEmail());
-//		System.out.println(result);
-
-		String json = objectMapper.writeValueAsString(result);
-		response.setContentType("text/html; charset=UTF-8");
-		response.getWriter().print(json);
-//		System.out.println(json);
 	}
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		MemberService dao = new MemberServiceImpl();
+		MemberVO vo = new MemberVO();
+		HttpSession session = request.getSession();
+
+		vo.setMemberId(request.getParameter("memberId"));
+		vo.setMemberEmail(request.getParameter("memberEmail"));
+		vo = dao.memberSearchPass(vo);
+
+		System.out.println(vo);
 		
+		if (vo != null) {
+			session.setAttribute("searchId", vo.getMemberId());
+			session.setAttribute("searchEmail", vo.getMemberEmail());
+
+			response.sendRedirect("passwordsearch.do");
+		} else {
+			System.out.println("err");
+		}
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		doGet(request, response);
 	}
 
